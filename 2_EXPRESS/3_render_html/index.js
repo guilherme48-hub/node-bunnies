d2e28b79809e8fba9e517c1419e86195829b8b60
gui gomes <guistsgomes07@gmail.com>
2023-09-29 10:05:28 -0300

    aula nodemon

---
+++ 2_EXPRESS/3_render_html/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express ()
+
+const caminho = path.join(__dirname,'templates')
+
+app.get('/',(requisicao,resposta) => {
+    resposta.sendFile(`${caminho}/index.html`)
+})
+
+app.listen(3000,() =>{
+    console.log("servidor rodando na porta 3000")
+})
